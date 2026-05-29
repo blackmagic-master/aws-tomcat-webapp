58b7fa96e40e912a74478097ed19ad28ebc13af4
blackmagic-master <112019925+blackmagic-master@users.noreply.github.com>
2026-05-29 19:39:03 +0300

    config modification

--- aws-webapp.py
+++ aws-webapp.py
@@ -1,9 +1,8 @@
 import json
-import src
-import src.setup
+import source.setup as setup
 
 config_file = open('config.json', 'r')
 config = json.load(config_file)
 config_file.close()
 
-src.setup.init(config)
+setup.init(config)
